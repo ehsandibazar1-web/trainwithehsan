0fc5e23be6499a33a75c581bebabd19e57626992
ehsandibazar1-web <ehsan.dibazar1@gmail.com>
2026-07-13 12:44:48 +0000

    Fix activitylog v5 namespace

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use Spatie\Activitylog\LogOptions;
-use use Spatie\Activitylog\Models\Concerns\LogsActivity;
+use Spatie\Activitylog\Models\Concerns\LogsActivity;
 
 class Article extends Model
 {
